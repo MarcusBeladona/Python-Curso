256903ef12710199a475b577aded2f1cac2f9899
Marcus Beladona <mv3265@gmail.com>
2019-11-26 14:18:37 -0300

    For & While & List

--- Introdução/App.py
+++ Introdução/App.py
@@ -42,8 +42,6 @@ idade = int(input("Digite idade: "))
 
 print(f"Olá {nome}, sua idade é {idade} e você nasceu em {2019-idade}.")
 
-"""
-
 a = 2
 b = 1
 
@@ -62,3 +60,101 @@ else:
     print("Não")
 
 #print("sim") if a > b or a == b else print("não")
+
+nome = input("Seu nome: ")
+
+if 4 < len(nome):
+    print("Nome pequeno.")
+elif 4 < len(nome) < 6:
+    print("Nome normal.")
+elif len(nome) >= 6:
+    print("Nome grande.")
+else:
+    print("Não consegui dizer.")
+"""
+
+#nome = "Marcus"
+# print(nome[:4:2])
+
+'''
+
+# LAÇO WHILE
+
+x = 0
+
+while x < 6:
+
+    if x == 3:
+        x += 1
+        continue
+
+    print(x)
+    x += 1
+
+string = "string"
+'''
+
+# [0:1:2] 0 = início, 1 = fim, 2 = quantidade de pulos.
+# string[::-1] = gnirts : A string retorna ao contrário.
+
+# Toda string tem código ao contrário também, começando do -1.
+#  0  1  2  3  4  5
+#  s  t  r  i  n  g
+# -6 -5 -4 -3 -2 -1
+
+# LAÇO FOR IN
+
+'''
+texto = "João Victor"
+
+for c in texto:
+    print(c)
+
+# range(start,stop,step)
+for numero in range(1, 51, 1):
+    if numero % 2 == 0:
+        print(numero)
+
+# pra contar as iterações
+for n, numero in enumerate(texto):
+    print(n, numero)
+
+texto = "Oh lord of love"
+new = ""
+
+for letra in texto:
+    if letra == "o":
+        new += letra.upper()
+    elif letra == "l":
+        new += letra.upper()
+    else:
+        new += letra
+
+print(new)
+
+
+
+# LISTAS
+
+# Aceitam misturas de diferentes tipos de variáveis.
+lista = [1, 2, 3, 4]
+
+print(lista[3])
+
+# print(lista[:-4:-1])
+
+lista.append(5)
+
+lista.insert(5, 6)
+lista.pop(5)
+
+del(lista[4])
+
+print(lista)
+print(max(lista), min(lista))
+
+# É possível usar lista[0:0:0] em listas também;
+
+lista_dois = list(range(1, 11))
+print(lista_dois)
+'''
